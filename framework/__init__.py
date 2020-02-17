5d6f90f9b48fd6fe338ff72cd4a36a4372c18053
Jack Horsburgh <s1627278@sms.ed.ac.uk>
2020-02-17 14:09:39 +0000

    Add script for baseline

--- framework/__init__.py
+++ framework/__init__.py
@@ -1,7 +1,19 @@
 
 import os 
+# import argparse
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
+# TODO: Check if on MLP or not
+# parser = argparse.ArgumentParser(description='Process some integers.')
+# parser.add_argument('mlp-cluster', metavar='M', type=int, nargs='+',
+#                     help='1 for mlp cluster, otherwise 0', default=0)
+# args = parser.parse_args()
+# if args.mlp-cluster == 1:
+#     DATASET_PATH = os.getenv('DATASET_DIR')
+#     if DATASET_PATH is None:
+#         raise RuntimeError("Dataset path is None on MLP cluster")
+# else:
 DATASET_PATH = os.path.join(dir_path, "..", "datasets")
 if not os.path.exists(DATASET_PATH):
     os.mkdir(DATASET_PATH)
