d1889c629bfafc53e511cf508aee7e4c3a6f18b0
Jack Horsburgh <jackhorse1000@gmail.com>
2020-03-07 12:08:43 +0000

    add skeleton

---
+++ framework/networks/calc_feature_vec.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+def get_model(model_name: str) -> nn.Module:
+
+
+
+if __name__ == '__main__':
+    model_name = 'arc_face'
+    model = get_model(model_name)
+
+    
