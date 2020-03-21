87c5a2be0d70cb936d10371a705c99f3fe11e532
Jack Horsburgh <s1627278@sms.ed.ac.uk>
2020-03-21 17:33:18 +0000

    add ensemble main

--- framework/main.py
+++ framework/main.py
@@ -3,7 +3,7 @@ import tqdm
 import pickle 
 import json
 import numpy as np
-from sklearn.metrics import roc_auc_score
+
 from datetime import datetime
 from pprint import pprint
 
@@ -11,27 +11,10 @@ import torch
 from torch.utils.data import DataLoader
 
 from framework import DATASET_PATH, RESULTS_PATH, MODEL_PATH
-from framework.networks.linear_layer import MLP
-from framework.data_provider import FIW
 from framework.config.config_reader import ConfigReader
 from framework.utils.gen_utils import get_data_loader
+from framework.utils.experiment_utils import get_model, eval, get_datasets, check_network_name
 
-def check_network_name(network_name: str):
-    networks = ['arc_face', 'vgg_face', 'sphere_face', 'vgg_face2']
-    if network_name not in networks:
-        raise RuntimeError(f'Unkown network name {network_name}')
-
-def get_model(model_settings: dict):
-    model_name = model_settings['model_name']
-    model = None
-    
-    # Implement for other models
-    if model_name == 'MLP':
-        model = MLP(model_settings)
-    else:
-        raise RuntimeError(f'unkown model name = {model_name}')
-
-    return model
 
 
 def train(model, train_dataset, training_settings, validation_dataset = None):
@@ -102,72 +85,16 @@ def train(model, train_dataset, training_settings, validation_dataset = None):
     progress.close()
     return train_dict
 
-def get_datasets(network_name: str):
-    # NOTE: Netowrk name is required to known which network produces the feature vectors
-    csv_path = os.path.join(DATASET_PATH, 'fiw', 'tripairs', '5_cross_val.csv')
-    mappings_path = os.path.join(RESULTS_PATH, f'mappings_{network_name}.pickle')
-    train_folds = [1, 2, 3]
-    validation_folds = [4]
-    test_folds = [5]
-    train_dataset = FIW(csv_path, mappings_path, train_folds)
-    validation_dataset = FIW(csv_path, mappings_path, validation_folds)
-    test_dataset = FIW(csv_path, mappings_path, test_folds)
-    vec_length = list(train_dataset.__getitem__(0)[0].size())[0]
-    return train_dataset, validation_dataset, test_dataset, vec_length
-
-def eval(model, test_dataset):
-    # Evals the model based on test set to produce metrics for use
-    model.eval()
-
-    cuda = model._is_on_cuda()
-    dataloader = get_data_loader(test_dataset, 1, cuda)
-    y_hats = []
-    ys = []
-    with torch.no_grad():
-        for i, data in enumerate(dataloader, 1):
-            # Get batch and a features length
-            x = data[0].to(model._device(), dtype=torch.float)
-            y = data[1].to(model._device(), dtype=torch.long)
-
-            y_hat = model(x)
-            pred_target = torch.max(y_hat, dim=1)[1]
-            y_hats.append(pred_target.cpu().numpy())
-            ys.append(y.cpu().numpy())
-    auc = roc_auc_score(np.array(ys), np.array(y_hats))
-
-
-    y_hats_fmd = []
-    y_hats_fms = []
-    ys_fmd = []
-    ys_fms = []
-    for idx in range(len(test_dataset.dataset)):
-        # Get type
-        type_relation = test_dataset.dataset.iloc[idx].type
-        tripair = test_dataset._get_tripair(idx)
-        label =  test_dataset._get_label(idx)
-
-        x = tripair.to(model._device(), dtype=torch.float).view(1, -1)
-        y = label
-
-        y_hat = model(x)
-        pred_target = torch.max(y_hat, dim=1)[1]
-        if type_relation == 'fmd':
-            y_hats_fmd.append(pred_target.cpu().numpy())
-            ys_fmd.append(y)
-        elif type_relation == 'fms':
-            y_hats_fms.append(pred_target.cpu().numpy())
-            ys_fms.append(y)
-        else:
-            raise RuntimeError('Unkown relationship type = {}'.format(type_relation))
-    fmd_auc = roc_auc_score(np.array(ys_fmd), np.array(y_hats_fmd))
-    fms_auc = roc_auc_score(np.array(ys_fms), np.array(y_hats_fms))
-    return {'auc': auc,
-            'fmd_auc': fmd_auc,
-            'fms_auc': fms_auc}
-
 def run_experiment(profile_name: str):
     # Get experiment settings
     config_data = ConfigReader(profile_name).config_data
+    if config_data['save_model']:
+        model_name = os.path.join(MODEL_PATH, config_data['experiment_name'] + '.pt')
+        if os.path.exists(model_name):
+            if not config_data['overwrite_model']:
+                raise Warning("Saving model when another model already exists, {}".format(model_name))
+            else:
+                print('Warning: Overwriting existing model. {}'.format(model_name))
 
     results_dict = {}
     results_dict['config_data'] = config_data
@@ -196,7 +123,6 @@ def run_experiment(profile_name: str):
     results_dict['test_acc'] = result
     print('test_result = {}'.format(result))
 
-    # TODO: Maybe save model
     # Save experiment results
     if config_data['save_results']:
         experiment_name = config_data['experiment_name'] + '_' + str(datetime.now())
@@ -205,8 +131,8 @@ def run_experiment(profile_name: str):
             pickle.dump(results_dict, f)
     
     if config_data['save_model']:
-        model_name = os.path.join(MODEL_PATH, config_data['experiment_name'])
-        model.save_state_dict(model_name)
+        model_name = os.path.join(MODEL_PATH, config_data['experiment_name'] + '.pt')
+        torch.save(model.state_dict(), model_name)
 
     pprint(results_dict)
 
---
+++ framework/main_ensemble.py
@@ -0,0 +1,117 @@
+import os
+import json
+from typing import List
+import numpy as np
+from sklearn.metrics import roc_auc_score
+
+import torch
+
+from framework import MODEL_PATH
+from framework.utils.experiment_utils import get_model, check_network_name, get_datasets, get_data_loader
+from framework.config.config_reader import ConfigReader
+
+def high_score(scores):
+    # np.reshape(scores, (scores.shape[0], scores.shape[1], 2))
+    print('scores shape: ', scores.size())
+    result = torch.sum(scores, dim=0)
+    print(result.size())
+    result = torch.max(result, dim=0)[1]
+    print(result.size())
+    # scores_0 = np.sum(scores, axis=0)
+    # print('0: ', scores_0.shape)
+    # scores_1 = np.sum(scores, axis=1)
+    # print('1: ', scores_1.shape)
+    # scores_2 = np.sum(scores, axis=2)
+    # print('2: ', scores_2.shape)
+    
+    # for score in scores:
+        # print(score)
+        # break
+    return result.cpu().numpy()
+
+def get_ensemble(ensemble_name: str):
+    if ensemble_name == "high_score":
+        return high_score
+    else:
+        raise RuntimeError('Unkown ensemble name {}'.format(ensemble_name))
+    pass
+
+
+def load_model(model_path, model_settings):
+    model = get_model(model_settings)
+    model_path = os.path.join(MODEL_PATH, model_path + '.pt')
+    if not os.path.exists(model_path):
+        raise RuntimeError('Unkown {} model path'.format(model_path))
+    model.load_state_dict(torch.load(model_path))
+    return model
+
+def get_scores(model, dataset):
+    model.eval()
+    cuda = model._is_on_cuda()
+    dataloader = get_data_loader(dataset, 1, cuda)
+
+    scores_list = []
+    with torch.no_grad():
+        for i in range(len(dataset)):
+            x = dataset[i][0].to(model._device(), dtype=torch.float).view(1, -1)
+            # y = data[1].to(model._device(), dtype=torch.long)
+            y_hat = model(x)
+            scores_list.append(y_hat)
+    return torch.stack(scores_list)
+
+def eval(pred_targets, dataset):
+    targets = []
+    for i in range(len(dataset)):
+        y = dataset[i][1]
+        targets.append(y)
+    auc = roc_auc_score(np.array(targets), np.array(pred_targets))
+    return auc
+    
+
+def run_experiment(ensemble_name: str, model_profiles: List[str]):
+    models = []
+    test_datasets = []
+    for profile in model_profiles:
+        config_data = ConfigReader(profile).config_data
+
+        # Get test datasets for each model
+        network_name = config_data['data_settings']['network_name']
+        check_network_name(network_name)
+        train_dataset, validation_dataset, test_dataset, vec_length = get_datasets(network_name)
+        test_datasets.append(test_dataset)
+
+        model_settings = config_data['model_settings']
+        model_settings['input_size'] = vec_length
+        experiment_name = config_data['experiment_name']
+        models.append(load_model(experiment_name, model_settings))
+
+    # A list of lists of scores
+    # NOTE: scores[0] -> scores of model 0 in models list 
+    scores = torch.stack([get_scores(model, test_dataset) for model, test_dataset in zip(models, test_datasets)])
+
+    # TODO: Use ensemble method with test outputs, this should ouput a list of targets
+    ensemble_method = get_ensemble(ensemble_name)
+    pred_targets = ensemble_method(scores)
+    # TODO: Evaluate the model on the ensemble solution
+    auc = eval(pred_targets, test_dataset)
+    print('test dataset auc = {}'.format(auc))
+
+
+if __name__ == '__main__':
+    # Get profile name from profile file
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    profile_path = os.path.join(dir_path, "config", "profile.json")
+    if not os.path.exists(profile_path):
+        raise RuntimeError('Please add a profile.json file in MLP-Project/config/profile.json. \n' +
+                            '       It should contain one line like this: {\"profile\": "profile_name"}')
+    with open(profile_path, 'r') as f:
+        profile_name = json.load(f)["profile"]
+    
+    # NOTE This should be a list of profiles you want to use. The models are expected to be saved
+    model_profiles = ['DROP_TWO_DEC_arc', 'DROP_TWO_DEC_vgg2', 'DROP_TWO_DEC_vgg', 'DROP_TWO_DEC_sphere']
+    ensemble_name = 'high_score'
+    if model_profiles == []:
+        raise RuntimeError('Please fill in model profiles')
+
+    run_experiment(ensemble_name, model_profiles)
+    
